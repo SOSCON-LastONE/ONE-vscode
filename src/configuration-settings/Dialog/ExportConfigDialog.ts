cd6a80152bd370df5e3273c669d0a9a37f745f21
Yeseo Kang <yeeybook@naver.com>
2021-09-15 15:49:47 +0900

    feat: add default name of completed .cfg file
    ONE-vscode-DCO-1.0-Signed-off-by: Yeseo Kang <yeeybook@naver.com>

--- src/configuration-settings/Dialog/ExportConfigDialog.ts
+++ src/configuration-settings/Dialog/ExportConfigDialog.ts
@@ -1,6 +1,4 @@
-import { DefaultDeserializer } from 'v8';
 import * as vscode from 'vscode';
-import { ResourceLimits } from 'worker_threads';
 
 export interface Tool{
     type: string;
@@ -18,38 +16,27 @@ export function exportConfig(oneToolList: any): void{
     const config = new ConfigPareser();
     
     config.addSection('one-build');
-    console.log('dddddddddddd');
-    console.log(oneToolList[0]);
-    console.log('dddddaaaaaadddddddd');
-    console.log(oneToolList[1]);
 
     for(let i = 0; i < oneToolList.length; i++){
+      config.set('one-build', oneToolList[i].type, oneToolList[i].use);
+      if(oneToolList[i].use === false) continue;
       config.addSection(oneToolList[i].type);
-      console.log(oneToolList[i].type);
+      for(let j = 0; j < oneToolList[i].options.length; j++){
+        if(oneToolList[i].options[j].optionValue === false || oneToolList[i].options[j].optionValue === '') continue;
+        config.set(oneToolList[i].type, oneToolList[i].options[j].optionName, oneToolList[i].options[j].optionValue);
+      }
     }
-    config.set()
-    // Adding sections and adding keys
-    config.addSection('User');
-    config.set('User', 'token', 'some value');
-    config.set('User', 'exp', 'some value');
-
-    // With String Interpolation, %(key_name)s
-    config.addSection('MetaData');
-    config.set('MetaData', 'path', '/home/%(dir_name)s/');
-    config.set('MetaData', 'dir_name', 'me');
-
-    // config.write('my-cfg-file.cfg');
-    console.log('successfully wrote file!!!')
 
     const optionsForExportDialog: vscode.SaveDialogOptions = {
+        defaultUri: vscode.Uri.file('template.cfg'),
         filters: {
           'allFiles': ['*']
         }
       };
     vscode.window.showSaveDialog(optionsForExportDialog).then(fileUri => {
       if (fileUri) {
-        config.write(fileUri.path + '.cfg');
-        console.log('Selected file!!!: ' + fileUri.path + '.cfg');
+        config.write(fileUri.path);
+        console.log('Selected file!!!: ' + fileUri.path);
         }
     });
 }
